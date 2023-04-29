12939056ad2ffce2e6dbe7a8dbc53324b81922c0
Carlos Ramirez <karlossander92@gmail.com>
2023-04-28 20:38:22 -0500

    Primer Commit

---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [Route("api/autores")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            { 
+                new Autor() { Id = 1 , Nombre = "Carlos"},
+                new Autor() { Id = 2 , Nombre = "Angelica"},
+            };
+        }
+    }
+}
